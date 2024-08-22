7835a4ae1717fd9839a4801d3da8bdf98a6ae251
chap-zenith .h <chap36399@gmail.com>
2024-08-22 18:45:24 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x85E881e598d4357dd7f1b6421cD205663856200c'
+export const ProxyAdmin = '0x4724Ce295cDBe6eB9379F551DeBD04CA2dbcA588'
+export const SWTRImplementation = '0xe9Ecb8EF1EADEA4Ec98CE33Cc159fFa7698Ac950'
     
